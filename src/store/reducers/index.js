862d19066115cd5ffa001fd991952f43e131a67f
Claudio Junior <cso.junior1996@gmail.com>
2018-12-11 01:20:33 -0200

    Aula: Nova aplicação

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux';
 
-import todos from './todos';
-
-const reducers = combineReducers({ todos });
+const reducers = combineReducers({ example: () => [] });
 
 export default reducers;
