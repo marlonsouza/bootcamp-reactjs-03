6627f4c42b68cf156aac5b1554f2c69d524ba096
Claudio Junior <cso.junior1996@gmail.com>
2018-12-11 01:27:05 -0200

    Aula: Interface da aplicação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    const { repositoryInput } = this.state;
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong>
+              {' '}
+(A declarative, efficient, and flexible JavaScript
+              library for building user interfaces.)
+            </p>
+            <a href="https://github.com/facebook/react">Acessar</a>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
