2fa36b282d5ad0746d6de76e564349c1639a51d2
Claudio Junior <cso.junior1996@gmail.com>
2018-12-11 00:53:12 -0200

    Aula: Lista de todos

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import TodoList from './TodoList';
+
 const App = () => (
   <Provider store={store}>
-    <h1>Hello Rocketseat!</h1>
+    <TodoList />
   </Provider>
 );
 
---
+++ src/store/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import todos from './todos';
+
+const reducers = combineReducers({ todos });
+
+export default reducers;
