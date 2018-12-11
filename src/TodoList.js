b8e6327463d267af8cc4ba56594462d910bec829
Claudio Junior <cso.junior1996@gmail.com>
2018-12-11 01:05:29 -0200

    Aula: Remover todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,11 +5,16 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as TodoActions from './store/actions/todos';
 
-const TodoList = ({ todos, addTodo }) => (
+const TodoList = ({ todos, addTodo, removeTodo }) => (
   <Fragment>
     <ul>
       {todos.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          {todo.text}
+          <button type="button" onClick={() => removeTodo(todo.id)}>
+            Remover
+          </button>
+        </li>
       ))}
     </ul>
     <button type="button" onClick={() => addTodo('Novo Todo')}>
@@ -26,6 +31,7 @@ TodoList.propTypes = {
     }),
   ).isRequired,
   addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
