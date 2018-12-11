33831fc1563d20cf22d4df37b4ee222e1a052d18
Claudio Junior <cso.junior1996@gmail.com>
2018-12-11 00:58:19 -0200

    Aula: Adicionar todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 
-const TodoList = ({ todos }) => (
-  <ul>
-    {todos.map(todo => (
-      <li key={todo.id}>{todo.text}</li>
-    ))}
-  </ul>
+const TodoList = ({ todos, addTodo }) => (
+  <Fragment>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+    <button type="button" onClick={() => addTodo('Novo Todo')}>
+      Adicionar
+    </button>
+  </Fragment>
 );
 
 TodoList.propTypes = {
@@ -18,10 +23,18 @@ TodoList.propTypes = {
       text: PropTypes.string,
     }),
   ).isRequired,
+  addTodo: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   todos: state.todos,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => ({
+  addTodo: text => dispatch({ type: 'ADD_TODO', payload: { text } }),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TodoList);
